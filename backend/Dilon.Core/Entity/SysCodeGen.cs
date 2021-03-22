95b23fd9b1ab256ddd928eb7adaafdb9c3397588
zuohuaijun <zuohuaijun@163.com>
2021-03-23 01:09:49 +0800

    :construction: 增加代码生成器初始化类

---
+++ backend/Dilon.Core/Entity/SysCodeGen.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dilon.Core.Entity
+{
+    /// <summary>
+    /// 代码生成表
+    /// </summary>
+    [Table("sys_code_gen")]
+    public class SysCodeGen : DEntityBase
+    {
+        /// <summary>
+        /// 作者姓名
+        /// </summary>
+        public string AuthorName { get; set; }
+
+        /// <summary>
+        /// 类名
+        /// </summary>
+        public string ClassName { get; set; }
+
+        /// <summary>
+        /// 是否移除表前缀
+        /// </summary>
+        public string TablePrefix { get; set; }
+
+        /// <summary>
+        /// 生成方式
+        /// </summary>
+        public string GenerateType { get; set; }
+
+        /// <summary>
+        /// 数据库表名
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// 包名
+        /// </summary>
+        public string PackageName { get; set; }
+
+        /// <summary>
+        /// 业务名（业务代码包名称）
+        /// </summary>
+        public string BusName { get; set; }
+
+        /// <summary>
+        /// 功能名（数据库表名称）
+        /// </summary>
+        public string TableComment { get; set; }
+    }
+}
---
+++ backend/Dilon.Core/Service/CodeGen/CodeGenService.cs
@@ -0,0 +1,18 @@
+﻿using Furion.DependencyInjection;
+using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dilon.Core.Service.CodeGen
+{
+    /// <summary>
+    /// 代码生成器服务
+    /// </summary>
+    [ApiDescriptionSettings(Name = "CodeGen", Order = 100)]
+    public class CodeGenService : IDynamicApiController, ITransient
+    {
+        public CodeGenService()
+        {
+
+        }
+    }
+}
