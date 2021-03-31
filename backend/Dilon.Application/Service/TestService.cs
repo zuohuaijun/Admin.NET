0886365d4b952f8a01f411d5fda067772ffaef4b
“kfz001” <1491897341@qq.com>
2021-04-01 01:14:13 +0800

    sqlsugar 示例

--- backend/Dilon.Application/Service/TestService.cs
+++ backend/Dilon.Application/Service/TestService.cs
@@ -1,5 +1,10 @@
 ﻿using Furion.DependencyInjection;
 using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Mvc;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Dilon.Application
 {
@@ -8,10 +13,43 @@ namespace Dilon.Application
     /// </summary>
     public class TestService : ITestService, IDynamicApiController, ITransient
     {
-        public TestService()
+        private readonly ISqlSugarRepository<Test> repository; // 仓储对象：封装简单的CRUD
+        private readonly SqlSugarClient db; // 核心对象：拥有完整的SqlSugar全部功能
+        public TestService(ISqlSugarRepository<Test> sqlSugarRepository)
         {
-
+            repository = sqlSugarRepository;
+            db = (SqlSugarClient)repository.Context;    // 推荐操作
         }
+
+
+
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("/test/init")]
+        public async Task InitData()
+        {
+            var data = new Test()
+            {
+                Id = Guid.NewGuid(),
+                Name = "测试数据",
+                CreateTime = DateTime.Now,
+                Text="你好"
+            };
+           await repository.InsertAsync(data);
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("/test/page")]
+        public async Task<List<Test>> GetList()
+        {
+           return await repository.Entities.ToListAsync();
+        }
+
 
         public string GetDescription()
         {
---
+++ backend/Dilon.Application/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Furion;
+using Microsoft.Extensions.DependencyInjection;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dilon.Application
+{
+    public class Startup : AppStartup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSqlSugar(new ConnectionConfig
+            {
+                ConnectionString = App.Configuration.GetSection("ConnectionStrings:DefaultConnection").Value ,
+                DbType = DbType.Sqlite,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute//从特性读取主键自增信息
+            },
+            db =>
+            {
+                db.MappingTables.Add(typeof(Test).Name, typeof(Test).Name);
+                db.CodeFirst.SetStringDefaultLength(200).InitTables(typeof(Test));
+                //处理日志事务
+                db.Aop.OnLogExecuting = (sql, pars) =>
+                {
+                    Console.WriteLine(sql);
+                    Console.WriteLine(string.Join(",", pars?.Select(it => it.ParameterName + ":" + it.Value)));
+                    Console.WriteLine();
+                };
+            }); ; ;
+        }
+    }
+
+   
+
+}
