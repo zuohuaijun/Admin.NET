312540217c715fb1a8a1275e2ccb7a86b026c2a9
Jerry <chenjingchun@foxmail.com>
2021-04-06 23:42:13 +0800

    设置雪花id的workerId 开启Json序列化忽略循环引用 添加设置Json序列化的DefaultBufferSize属性

--- backend/Dilon.Web.Core/Startup.cs
+++ backend/Dilon.Web.Core/Startup.cs
@@ -3,15 +3,24 @@ using Furion;
 using Furion.Snowflake;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Dilon.Web.Core
 {
     public class Startup : AppStartup
     {
+        public IConfiguration _configuration { get; set; }
+
+        public Startup()
+        {
+            _configuration = App.GetService<IConfiguration>();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddJwt<JwtHandler>(enableGlobalAuthorize: true);
@@ -27,9 +36,14 @@ namespace Dilon.Web.Core
                     .AddInjectWithUnifyResult<XnRestfulResultProvider>()
                     .AddJsonOptions(options =>
                     {
+                        //options.JsonSerializerOptions.DefaultBufferSize = 10_0000;//返回较大数据数据序列化时会截断，原因：默认缓冲区大小（以字节为单位）为16384。
                         options.JsonSerializerOptions.Converters.AddDateFormatString("yyyy-MM-dd HH:mm:ss");
-                        //options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles; // 忽略循环引用
+                        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles; // 忽略循环引用
                     });
+
+            //设置雪花id的workerId，同一环境下每个实例workerId都应不一样
+            var workerId = ushort.Parse(_configuration["SnowId:WorkerId"]);//读配置
+            IDGenerator.SetIdGenerator(new IDGeneratorOptions { WorkerId = workerId });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
