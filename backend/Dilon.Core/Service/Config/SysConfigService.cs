ec0fcd37d1586b46e0f69b2d48346f3aa2fc113a
zuohuaijun <zuohuaijun@163.com>
2021-04-16 01:08:09 +0800

    😎修复系统配置编码分组bug

--- backend/Dilon.Core/Service/Config/SysConfigService.cs
+++ backend/Dilon.Core/Service/Config/SysConfigService.cs
@@ -40,7 +40,7 @@ namespace Dilon.Core.Service
             var configs = await _sysConfigRep.DetachedEntities
                                              .Where((name, u => EF.Functions.Like(u.Name, $"%{input.Name.Trim()}%")),
                                                     (code, u => EF.Functions.Like(u.Code, $"%{input.Code.Trim()}%")),
-                                                    (groupCode, u => EF.Functions.Like(u.Code, $"%{input.GroupCode.Trim()}%")))
+                                                    (groupCode, u => u.GroupCode == input.GroupCode.Trim()))
                                              .Where(u => u.Status != CommonStatus.DELETED).OrderBy(u => u.GroupCode)
                                              .ToPagedListAsync(input.PageNo, input.PageSize);
             return XnPageResult<SysConfig>.PageResult(configs);
