3a86eb74170b379852e79bbf4c33ac650992baac
柏明昌 <705170100@qq.com>
2021-04-15 09:15:47 +0800

    完善 DefaultDbContext

--- backend/Dilon.EntityFramework.Core/DbContexts/DefaultDbContext.cs
+++ backend/Dilon.EntityFramework.Core/DbContexts/DefaultDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Dilon.Core;
+using Dilon.Core.Entity;
 using Dilon.Core.Service;
 using Furion;
 using Furion.DatabaseAccessor;
@@ -28,33 +29,63 @@ namespace Dilon.EntityFramework.Core
         {
             // 获取当前事件对应上下文
             var dbContext = eventData.Context;
-
-            // 获取所有新增和更新的实体
-            var entities = dbContext.ChangeTracker.Entries().Where(u => u.State == EntityState.Added || u.State == EntityState.Modified);
-
-            foreach (var entity in entities)
+            // 获取所有更改，删除，新增的实体，但排除审计实体（避免死循环）
+            var entities = dbContext.ChangeTracker.Entries()
+                                    .Where(u => u.Entity.GetType() != typeof(SysLogAudit) && u.Entity.GetType() != typeof(SysLogOp) && u.Entity.GetType() != typeof(SysLogVis) &&
+                                          (u.State == EntityState.Modified || u.State == EntityState.Deleted || u.State == EntityState.Added))
+                                    .ToList();
+            if (entities == null || entities.Count < 1) return;
+            // 判断是否是演示环境
+            var demoEnvFlag = App.GetService<ISysConfigService>().GetDemoEnvFlag().GetAwaiter().GetResult();
+            if (demoEnvFlag)
             {
-                if (entity.Metadata.ClrType.BaseType.Name != "DBEntityTenant")
-                {
-                    continue;
-                }
-                switch (entity.State)
-                {
-                    // 自动设置租户Id
-                    case EntityState.Added:
-                        entity.Property(nameof(Entity.TenantId)).CurrentValue = long.Parse(GetTenantId().ToString());
-                        break;
-                    // 排除租户Id
-                    case EntityState.Modified:
-                        entity.Property(nameof(Entity.TenantId)).IsModified = false;
-                        break;
-                }
-
-
+                var sysUser = entities.Find(u => u.Entity.GetType() == typeof(SysUser));
+                if (sysUser == null || string.IsNullOrEmpty((sysUser.Entity as SysUser).LastLoginTime.ToString())) // 排除登录
+                    throw Oops.Oh(ErrorCode.D1200);
             }
 
+            // 当前操作用户信息
+            var userId = App.User.FindFirst(ClaimConst.CLAINM_USERID)?.Value;
+            var userName = App.User.FindFirst(ClaimConst.CLAINM_ACCOUNT)?.Value;
+            foreach (var entity in entities)
+            {
+                if (entity.Entity.GetType().IsSubclassOf(typeof(DEntityBase)))
+                {
+                    var obj = entity.Entity as DEntityBase;
+                    if (entity.State == EntityState.Added)
+                    {
+                        obj.Id = IDGenerator.NextId();
 
+                        obj.CreatedTime = DateTimeOffset.Now;
+                        if (!string.IsNullOrEmpty(userId))
+                        {
+                            obj.CreatedUserId = long.Parse(userId);
+                            obj.CreatedUserName = userName;
+                        }
+                    }
+                    else if (entity.State == EntityState.Modified)
+                    {
+                        obj.UpdatedTime = DateTimeOffset.Now;
+                        obj.UpdatedUserId = long.Parse(userId);
+                        obj.UpdatedUserName = userName;
+                    }
+                }
+                else if (entity.Entity.GetType().IsSubclassOf(typeof(DBEntityTenant)))
+                {
+                    switch (entity.State)
+                    {
+                        // 自动设置租户Id
+                        case EntityState.Added:
+                            entity.Property(nameof(Entity.TenantId)).CurrentValue = long.Parse(GetTenantId().ToString());
+                            break;
+                        // 排除租户Id
+                        case EntityState.Modified:
+                            entity.Property(nameof(Entity.TenantId)).IsModified = false;
+                            break;
+                    }
+                }
 
+            }
         }
         protected override LambdaExpression FakeDeleteQueryFilterExpression(EntityTypeBuilder entityBuilder, DbContext dbContext, string isDeletedKey = null)
         {
