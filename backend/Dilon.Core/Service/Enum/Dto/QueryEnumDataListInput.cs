a52209a5f87c6426ec6298f87d17922eaa7f16bd
TaoRan <staoran@live.cn>
2021-04-17 16:10:50 +0800

    增加枚举值相关接口 增加反射工具类获取字段特性 增加枚举数据获取相关扩展方法

---
+++ backend/Dilon.Core/Service/Enum/Dto/QueryEnumDataListInput.cs
@@ -0,0 +1,18 @@
+﻿namespace Dilon.Core.Service
+{
+    /// <summary>
+    /// 枚举返回参数
+    /// </summary>
+    public class EnumDataOutput
+    {
+        /// <summary>
+        /// 字典Id
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 字典值
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
