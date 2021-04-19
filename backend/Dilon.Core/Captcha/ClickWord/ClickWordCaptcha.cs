6be5f6209859fcdb3451cb655b12237c727c1341
zuohuaijun <zuohuaijun@163.com>
2021-04-19 23:18:39 +0800

    😆解决验证码总是最后三位汉字的问题

--- backend/Dilon.Core/Captcha/ClickWord/ClickWordCaptcha.cs
+++ backend/Dilon.Core/Captcha/ClickWord/ClickWordCaptcha.cs
@@ -47,16 +47,12 @@ namespace Dilon.Core
 
             string bgImagesDir = Path.Combine(App.WebHostEnvironment.WebRootPath, "Captcha/Image");
             string[] bgImagesFiles = Directory.GetFiles(bgImagesDir);
-            //if (bgImagesFiles == null || bgImagesFiles.Length == 0)
-            //    throw Oops.Oh("背景图片文件丢失");
 
             // 字体来自：https://www.zcool.com.cn/special/zcoolfonts/
             string fontsDir = Path.Combine(App.WebHostEnvironment.WebRootPath, "Captcha/Font");
             string[] fontFiles = new DirectoryInfo(fontsDir)?.GetFiles()
                 ?.Where(m => m.Extension.ToLower() == ".ttf")
                 ?.Select(m => m.FullName).ToArray();
-            //if (fontFiles == null || fontFiles.Length == 0)
-            //    throw Oops.Oh("字体文件丢失");
 
             int imgIndex = random.Next(bgImagesFiles.Length);
             string randomImgFile = bgImagesFiles[imgIndex];
