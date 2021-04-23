455a6f9d09960c8b04a69be8d56322f169e252ac
zuohuaijun <zuohuaijun@163.com>
2021-04-23 23:03:50 +0800

    😁修复新分页错误

--- backend/Dilon.Core/Extension/XnPageResult.cs
+++ backend/Dilon.Core/Extension/XnPageResult.cs
@@ -3,14 +3,14 @@ using Mapster;
 
 namespace Dilon.Core
 {
-    //public class PageResult<T>
-    //{
-    //    public int PageNo { get; set; }
-    //    public int PageSize { get; set; }
-    //    public int TotalPage { get; set; }
-    //    public int TotalRows { get; set; }
-    //    public ICollection<T> Rows { get; set; }
-    //}
+    public class PageResult<T>
+    {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPage { get; set; }
+        public int TotalRows { get; set; }
+        public ICollection<T> Rows { get; set; }
+    }
 
     /// <summary>
     /// 小诺分页列表结果
@@ -31,16 +31,16 @@ namespace Dilon.Core
             };
         }
 
-        //public static PageResult<V> PageResult<V>(PagedList<T> page)
-        //{
-        //    return new PageResult<V>
-        //    {
-        //        PageNo = page.PageIndex,
-        //        PageSize = page.PageSize,
-        //        TotalPage = page.TotalPages,
-        //        TotalRows = page.TotalCount,
-        //        Rows = page.Items.Adapt<List<V>>(),
-        //    };
-        //}
+        public static PageResult<V> PageResult<V>(PagedList<T> page)
+        {
+            return new PageResult<V>
+            {
+                PageNo = page.PageIndex,
+                PageSize = page.PageSize,
+                TotalPage = page.TotalPages,
+                TotalRows = page.TotalCount,
+                Rows = page.Items.Adapt<List<V>>(),
+            };
+        }
     }
 }
