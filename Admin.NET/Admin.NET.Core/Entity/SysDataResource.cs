aa600678aaf4806f87261969192532aad389551d
zuohuaijun <zuohuaijun@163.com>
2022-06-05 18:47:06 +0800

    优化

--- Admin.NET/Admin.NET.Core/Entity/SysDataResource.cs
+++ Admin.NET/Admin.NET.Core/Entity/SysDataResource.cs
@@ -10,47 +10,47 @@ public class SysDataResource : EntityBase
     /// <summary>
     /// 父节点Id
     ///</summary>
-    [SugarColumn(ColumnName = "pid", ColumnDescription = "父Id")]
+    [SugarColumn(ColumnDescription = "父Id")]
     public long Pid { get; set; }
 
     /// <summary>
     /// 名称
     ///</summary>
-    [SugarColumn(ColumnName = "name", ColumnDescription = "名称", Length = 200)]
+    [SugarColumn(ColumnDescription = "名称", Length = 200)]
     [MaxLength(200)]
     public string Name { get; set; }
 
     /// <summary>
     /// 值
     ///</summary>
-    [SugarColumn(ColumnName = "value", ColumnDescription = "值", Length = 200)]
+    [SugarColumn(ColumnDescription = "值", Length = 200)]
     [MaxLength(200)]
     public string Value { get; set; }
 
     /// <summary>
     /// 节点编码
     ///</summary>
-    [SugarColumn(ColumnName = "code", ColumnDescription = "节点编码", Length = 100)]
+    [SugarColumn(ColumnDescription = "节点编码", Length = 100)]
     [MaxLength(100)]
     public string Code { get; set; }
 
     /// <summary>
     /// 排序
     ///</summary>
-    [SugarColumn(ColumnName = "order", ColumnDescription = "排序")]
+    [SugarColumn(ColumnDescription = "排序")]
     public int Order { get; set; }
 
     /// <summary>
     /// 备注
     ///</summary>
-    [SugarColumn(ColumnName = "remark", ColumnDescription = "备注", Length = 200)]
+    [SugarColumn(ColumnDescription = "备注", Length = 200)]
     [MaxLength(200)]
     public string Remark { get; set; }
 
     /// <summary>
     /// 状态
     ///</summary>
-    [SugarColumn(ColumnName = "status", ColumnDescription = "状态")]
+    [SugarColumn(ColumnDescription = "状态")]
     public StatusEnum Status { get; set; } = StatusEnum.Enable;
 
     /// <summary>
