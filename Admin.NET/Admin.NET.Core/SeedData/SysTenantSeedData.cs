adb12ecda76a7218b7f64a1c2440a6769ff91583
UOSCN <323121749@qq.com>
2022-09-30 06:39:30 +0000

    update Admin.NET/Admin.NET.Core/SeedData/SysTenantSeedData.cs. 数据规范处理
    Signed-off-by: UOSCN <323121749@qq.com>

--- Admin.NET/Admin.NET.Core/SeedData/SysTenantSeedData.cs
+++ Admin.NET/Admin.NET.Core/SeedData/SysTenantSeedData.cs
@@ -1,4 +1,4 @@
-﻿namespace Admin.NET.Core;
+namespace Admin.NET.Core;
 
 /// <summary>
 /// 系统租户表种子数据
@@ -13,8 +13,8 @@ public class SysTenantSeedData : ISqlSugarEntitySeedData<SysTenant>
     {
         return new[]
         {
-            new SysTenant{ Id=142307070918780, Name="租户1", AdminName="admin1", Host="www.dilon.vip", Email="zuohuaijun@163.com", Phone="18020030720", Connection="D://db//1.db", Remark="租户1", CreateTime=DateTime.Parse("2022-02-10 00:00:00") },
-            new SysTenant{ Id=142307070918781, Name="租户2", AdminName="admin2", Host="www.dilon.top", Email="515096995@qq.com", Phone="18020030720", Connection="D://db//2.db", Remark="租户2", CreateTime=DateTime.Parse("2022-02-10 00:00:00") },
+            new SysTenant{ Id=142307070918780, Name="默认租户", AdminName="Administrator", Host="www.dilon.vip", Email="zuohuaijun@163.com", Phone="18020030720", Connection="./Manager.db", Remark="管理租户", CreateTime=DateTime.Parse("2022-02-10 00:00:00") },
+            new SysTenant{ Id=142307070918781, Name="测试租户", AdminName="TestAdmin", Host="www.dilon.top", Email="515096995@qq.com", Phone="18020030720", Connection="./Test.db", Remark="测试租户", CreateTime=DateTime.Parse("2022-02-10 00:00:00") },
         };
     }
 }
