2e8a20a3bb54bf38015c8ea0368a50cc9452e517
UOSCN <323121749@qq.com>
2022-10-13 01:51:39 +0000

    update Admin.NET/Admin.NET.Core/SeedData/SysRoleSeedData.cs. 修改cmomand为user自然语言可读
    Signed-off-by: UOSCN <323121749@qq.com>

--- Admin.NET/Admin.NET.Core/SeedData/SysRoleSeedData.cs
+++ Admin.NET/Admin.NET.Core/SeedData/SysRoleSeedData.cs
@@ -1,4 +1,4 @@
-﻿namespace Admin.NET.Core;
+namespace Admin.NET.Core;
 
 /// <summary>
 /// 系统角色表种子数据
@@ -14,11 +14,11 @@ public class SysRoleSeedData : ISqlSugarEntitySeedData<SysRole>
         return new[]
         {
             new SysRole{ Id=252885263003721, Name="管理员", DataScope=DataScopeEnum.Dept_with_child, Code="admin", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="管理员", TenantId=142307070918780 },
-            new SysRole{ Id=252885263003722, Name="普通用户", DataScope=DataScopeEnum.Self, Code="common", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="普通用户", TenantId=142307070918780 },
+            new SysRole{ Id=252885263003722, Name="普通用户", DataScope=DataScopeEnum.Self, Code="user", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="普通用户", TenantId=142307070918780 },
             new SysRole{ Id=252885263003723, Name="游客", DataScope=DataScopeEnum.Define, Code="guest", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="游客", TenantId=142307070918780 },
 
             new SysRole{ Id=252885263003724, Name="管理员", DataScope=DataScopeEnum.Dept_with_child, Code="admin", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="管理员", TenantId=142307070918781 },
-            new SysRole{ Id=252885263003725, Name="普通用户", DataScope=DataScopeEnum.Self, Code="common", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="普通用户",  TenantId=142307070918781 },
+            new SysRole{ Id=252885263003725, Name="普通用户", DataScope=DataScopeEnum.Self, Code="user", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="普通用户",  TenantId=142307070918781 },
             new SysRole{ Id=252885263003726, Name="游客", DataScope=DataScopeEnum.Define, Code="guest", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), Remark="游客",  TenantId=142307070918781 },
         };
     }
