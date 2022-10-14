3eb6efd8d82edd4a67257df0ff3c3fa2d6d4c0e0
zuohuaijun <zuohuaijun@163.com>
2022-10-14 17:54:29 +0800

    修正打包样式丢失异常

--- vue-next-admin/vite.config.ts
+++ vue-next-admin/vite.config.ts
@@ -38,6 +38,7 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 		build: {
 			outDir: 'dist',
 			sourcemap: false,
+			cssCodeSplit: false,
 			chunkSizeWarningLimit: 1500,
 			rollupOptions: {
 				output: {
