30c85081028bee9e5469c804f2f3bf18f957e8af
zuohuaijun <zuohuaijun@163.com>
2022-10-20 18:16:27 +0800

    增加表单设计VForm页面

--- vue-next-admin/src/main.ts
+++ vue-next-admin/src/main.ts
@@ -7,7 +7,9 @@ import { i18n } from '/@/i18n/index';
 import other from '/@/utils/other';
 
 import ElementPlus from 'element-plus';
+import VForm3 from 'vform3-builds'; // 引入VForm 3库
 import 'element-plus/dist/index.css';
+import 'vform3-builds/dist/designer.style.css'; // 引入VForm3样式
 import '/@/theme/index.scss';
 import mitt from 'mitt';
 import VueGridLayout from 'vue-grid-layout';
@@ -17,14 +19,12 @@ const app = createApp(App);
 directive(app);
 other.elSvg(app);
 
-app.use(pinia).use(router).use(ElementPlus, { i18n: i18n.global.t }).use(i18n).use(VueGridLayout).mount('#app');
+app.use(pinia).use(router).use(ElementPlus, { i18n: i18n.global.t }).use(VForm3).use(i18n).use(VueGridLayout).mount('#app');
 
-const globalProperties={
-    mittBus: mitt(),
-    i18n
-}
+const globalProperties = {
+	mittBus: mitt(),
+	i18n,
+};
+
 // 必须合并vue默认的变量，否则有问题
-app.config.globalProperties = Object.assign(
-    app.config.globalProperties,
-    globalProperties
-);
+app.config.globalProperties = Object.assign(app.config.globalProperties, globalProperties);
