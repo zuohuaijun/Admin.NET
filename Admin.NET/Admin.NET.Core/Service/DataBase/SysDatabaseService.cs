5ca19a9c26b53ceea499d5ed24195484e854a7cb
zuohuaijun <zuohuaijun@163.com>
2022-12-03 21:22:14 +0800

    修复库表管理MySql不支持修改描述 https://gitee.com/zuohuaijun/Admin.NET/issues/I6477Q

--- Admin.NET/Admin.NET.Core/Service/DataBase/SysDatabaseService.cs
+++ Admin.NET/Admin.NET.Core/Service/DataBase/SysDatabaseService.cs
@@ -143,13 +143,12 @@ public class SysDatabaseService : IDynamicApiController, ITransient
         var db = _db.AsTenant().GetConnectionScope(input.ConfigId);
         db.DbMaintenance.CreateTable(input.TableName, columns, false);
 
-        if (db.CurrentConnectionConfig.DbType == SqlSugar.DbType.Sqlite) return;
-
-        db.DbMaintenance.AddTableRemark(input.TableName, input.Description);
-
         if (columns.Any(m => m.IsPrimarykey))
             db.DbMaintenance.AddPrimaryKey(input.TableName, columns.FirstOrDefault(m => m.IsPrimarykey).DbColumnName);
 
+        if (db.CurrentConnectionConfig.DbType == SqlSugar.DbType.Sqlite || db.CurrentConnectionConfig.DbType == SqlSugar.DbType.MySql)
+            return;
+        db.DbMaintenance.AddTableRemark(input.TableName, input.Description);
         input.DbColumnInfoList.ForEach(m =>
         {
             db.DbMaintenance.AddColumnRemark(m.DbColumnName, input.TableName, string.IsNullOrWhiteSpace(m.ColumnDescription) ? m.DbColumnName : m.ColumnDescription);
