eb0d985ea4a97ff29dcc2afb34f31bc898e252db
zuohuaijun <zuohuaijun@163.com>
2023-02-03 00:13:53 +0800

    😉1、优化默认租户种子数据（与默认数据库保持一致）  2、升级Furion v4.8.5.6

--- Admin.NET/Admin.NET.Core/SeedData/SysTenantSeedData.cs
+++ Admin.NET/Admin.NET.Core/SeedData/SysTenantSeedData.cs
@@ -12,9 +12,11 @@ public class SysTenantSeedData : ISqlSugarEntitySeedData<SysTenant>
     [IgnoreUpdate]
     public IEnumerable<SysTenant> HasData()
     {
+        var defaultDbConfig = App.GetOptions<DbConnectionOptions>().ConnectionConfigs[0];
+
         return new[]
         {
-            new SysTenant{ Id=1300000000001, OrgId=1300000000101, UserId=1300000000111, Host="www.dilon.vip", TenantType=TenantTypeEnum.Id, DbType=SqlSugar.DbType.Sqlite, Connection="DataSource=./Admin.NET.db", ConfigId=SqlSugarConst.ConfigId, Remark="系统默认", CreateTime=DateTime.Parse("2022-02-10 00:00:00") },
+            new SysTenant{ Id=1300000000001, OrgId=1300000000101, UserId=1300000000111, Host="www.dilon.vip", TenantType=TenantTypeEnum.Id, DbType=defaultDbConfig.DbType, Connection=defaultDbConfig.ConnectionString, ConfigId=SqlSugarConst.ConfigId, Remark="系统默认", CreateTime=DateTime.Parse("2022-02-10 00:00:00") },
         };
     }
 }
