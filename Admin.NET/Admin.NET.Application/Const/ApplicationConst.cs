17ab25b1a767925f89e2f03012ee5e88a31b45ef
zuohuaijun <zuohuaijun@163.com>
2023-02-15 22:25:59 +0800

    🤓细节优化：1、调整默认端口  2、优化代码生成模板

---
+++ Admin.NET/Admin.NET.Application/Const/ApplicationConst.cs
@@ -0,0 +1,12 @@
+﻿namespace Admin.NET.Application.Const;
+
+/// <summary>
+/// 业务应用相关常量
+/// </summary>
+public class ApplicationConst
+{
+    /// <summary>
+    /// API分组名称
+    /// </summary>
+    public const string GroupName = "业务应用";
+}
--- Admin.NET/Admin.NET.Application/GlobalUsings.cs
+++ Admin.NET/Admin.NET.Application/GlobalUsings.cs
@@ -1,2 +1,8 @@
-﻿global using Furion;
+﻿global using Admin.NET.Core;
+global using Furion;
+global using Furion.DependencyInjection;
+global using Furion.DynamicApiController;
+global using Mapster;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.DependencyInjection;
+global using System.Threading.Tasks;
