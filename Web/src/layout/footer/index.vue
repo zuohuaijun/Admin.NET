20e461c9ede67651e083074f6de8b0d280f2370c
zuohuaijun <zuohuaijun@163.com>
2023-06-09 14:56:32 +0800

    😉前端页脚标注修改

--- Web/src/layout/footer/index.vue
+++ Web/src/layout/footer/index.vue
@@ -2,7 +2,7 @@
 	<div class="layout-footer pb15">
 		<div class="layout-footer-warp">
 			<div>Admin.NET</div>
-			<div class="mt5">Copyright © 2022 ZUOHUAIJUN All rights reserved.</div>
+			<div class="mt5">Copyright © 2023 Daming All rights reserved.</div>
 		</div>
 	</div>
 </template>
