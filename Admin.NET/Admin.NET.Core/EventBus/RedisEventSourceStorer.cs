65848d19904525d959e70865d6f2238f598c322b
zuohuaijun <zuohuaijun@163.com>
2023-07-16 15:28:28 +0800

    😉1、调整驼峰命名显示 2、升级所有依赖 3、增加文件头开源许可说明

--- Admin.NET/Admin.NET.Core/EventBus/RedisEventSourceStorer.cs
+++ Admin.NET/Admin.NET.Core/EventBus/RedisEventSourceStorer.cs
@@ -1,4 +1,13 @@
-﻿using System.Threading.Channels;
+﻿// 麻省理工学院许可证
+//
+// 版权所有 (c) 2021-2023 zuohuaijun，大名科技（天津）有限公司  联系电话/微信：18020030720  QQ：515096995
+//
+// 特此免费授予获得本软件的任何人以处理本软件的权利，但须遵守以下条件：在所有副本或重要部分的软件中必须包括上述版权声明和本许可声明。
+//
+// 软件按“原样”提供，不提供任何形式的明示或暗示的保证，包括但不限于对适销性、适用性和非侵权的保证。
+// 在任何情况下，作者或版权持有人均不对任何索赔、损害或其他责任负责，无论是因合同、侵权或其他方式引起的，与软件或其使用或其他交易有关。
+
+using System.Threading.Channels;
 
 namespace Admin.NET.Core;
 
@@ -50,7 +59,6 @@ public sealed class RedisEventSourceStorer : IEventSourceStorer, IDisposable
         // 创建消息订阅者
         _eventConsumer = new EventConsumer<ChannelEventSource>(_redis, _routeKey);
 
-
         // 订阅消息写入 Channel
         _eventConsumer.Received += (send, cr) =>
         {
@@ -91,7 +99,6 @@ public sealed class RedisEventSourceStorer : IEventSourceStorer, IDisposable
             // 异步发布
             await Task.Factory.StartNew(() =>
             {
-
                 queue.Add(source);
             }, cancellationToken, TaskCreationOptions.LongRunning, System.Threading.Tasks.TaskScheduler.Default);
         }
@@ -113,7 +120,6 @@ public sealed class RedisEventSourceStorer : IEventSourceStorer, IDisposable
         var eventSource = await _channel.Reader.ReadAsync(cancellationToken);
         return eventSource;
     }
-
 
     /// <summary>
     /// 释放非托管资源
