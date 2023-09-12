3eb6c65c053c58971dc09702b860284eda9a1c4d
zuohuaijun <zuohuaijun@163.com>
2023-09-12 15:39:12 +0800

    😁排除sqlite增加主键、表备注、列备注

--- Admin.NET/Admin.NET.Core/Service/DataBase/SysDatabaseService.cs
+++ Admin.NET/Admin.NET.Core/Service/DataBase/SysDatabaseService.cs
@@ -154,6 +154,9 @@ public class SysDatabaseService : IDynamicApiController, ITransient
         var db = _db.AsTenant().GetConnectionScope(input.ConfigId);
         db.DbMaintenance.CreateTable(input.TableName, columns, false);
 
+        if (db.CurrentConnectionConfig.DbType == SqlSugar.DbType.Sqlite)
+            return;
+
         if (columns.Any(m => m.IsPrimarykey))
             db.DbMaintenance.AddPrimaryKey(input.TableName, columns.FirstOrDefault(m => m.IsPrimarykey).DbColumnName);
 
