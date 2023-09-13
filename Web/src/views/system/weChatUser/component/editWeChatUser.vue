4bf0e84589aa5c2483195cdbeefe2dc4c008f0dc
zuohuaijun <zuohuaijun@163.com>
2023-09-13 09:32:09 +0800

    😁代码修复

--- Web/src/views/system/weChatUser/component/editWeChatUser.vue
+++ Web/src/views/system/weChatUser/component/editWeChatUser.vue
@@ -47,7 +47,7 @@ const state = reactive({
 const openDialog = (row: any) => {
 	state.ruleForm = JSON.parse(JSON.stringify(row));
 	state.isShowDialog = true;
-	state.isShowDialog = true;
+	ruleFormRef.value?.resetFields();
 };
 
 // 关闭弹窗
