3a70f7d48bb8d2f8fb8138f77c4562c79b9c3bab
yhli <youhua.li@avepoint.com>
2023-09-29 11:35:52 +0800

    细化学历枚举， 添加婚姻枚举

---
+++ Admin.NET/Admin.NET.Core/Enum/MaritalStatusEnum.cs
@@ -0,0 +1,37 @@
+namespace Admin.NET.Core;
+
+/// <summary>
+/// 婚姻状况枚举
+/// </summary>
+[Description("婚姻状况枚举")]
+public enum MaritalStatusEnum
+{
+    /// <summary>
+    /// 未知
+    /// </summary>
+    [Description("未知")]
+    None = 0,
+    /// <summary>
+    /// 未婚
+    /// </summary>
+    [Description("未婚")]
+    UnMarried = 1,
+
+    /// <summary>
+    /// 已婚
+    /// </summary>
+    [Description("已婚")]
+    Married = 2,
+
+    /// <summary>
+    /// 离异
+    /// </summary>
+    [Description("离异")]
+    Divorce = 3,
+
+    /// <summary>
+    /// 丧偶
+    /// </summary>
+    [Description("丧偶")]
+    Widowed = 4,
+}
