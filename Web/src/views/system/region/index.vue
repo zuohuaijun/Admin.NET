e48b7adc5e06cc621956e553f19c581df0d23285
许俊杰 <xjj0906@vip.qq.com>
2024-02-06 21:51:12 +0800

    feat: 优化机构、行政区域、账号管理界面显示

--- Web/src/views/system/region/index.vue
+++ Web/src/views/system/region/index.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="sys-region-container">
-		<el-row :gutter="8" style="width: 100%">
+		<el-row :gutter="8" style="width: 100%; flex: 1">
 			<el-col :span="6" :xs="24">
 				<RegionTree ref="regionTreeRef" @node-click="nodeClick" />
 			</el-col>
 
-			<el-col :span="18" :xs="24">
+			<el-col :span="18" :xs="24" style="display: flex; flex-direction: column">
 				<el-card shadow="hover" :body-style="{ paddingBottom: '0' }">
 					<el-form :model="state.queryParams" ref="queryForm" :inline="true">
 						<el-form-item label="行政名称">
