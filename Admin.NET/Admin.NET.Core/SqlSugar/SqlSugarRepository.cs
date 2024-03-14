a0e12a36252979cd7d2faa0bcdadcc299627bad4
zuohuaijun <zuohuaijun@163.com>
2024-03-15 00:56:19 +0800

    😁1、优化仓储  2、升级Furion和SqlSugar最新版本

--- Admin.NET/Admin.NET.Core/SqlSugar/SqlSugarRepository.cs
+++ Admin.NET/Admin.NET.Core/SqlSugar/SqlSugarRepository.cs
@@ -12,7 +12,7 @@ public class SqlSugarRepository<T> : SimpleClient<T> where T : class, new()
 {
     public SqlSugarRepository()
     {
-        var iTenant = App.GetRequiredService<ISqlSugarClient>().AsTenant();
+        var iTenant = SqlSugarSetup.ITenant;
         base.Context = iTenant.GetConnectionScope(SqlSugarConst.MainConfigId);
 
         // 若实体贴有多库特性，则返回指定库连接
